488414eca06be44ed302fec7381d2b70029953a2
Kirill Shepelev <kirill@webant.ru>
2019-03-04 19:35:59 +0300

    Commit not anything else

--- app/src/main/java/com/shepelevkirill/rksi/view/adapters/ScheduleAdapter.kt
+++ app/src/main/java/com/shepelevkirill/rksi/view/adapters/ScheduleAdapter.kt
@@ -8,9 +8,14 @@ import com.shepelevkirill.rksi.R
 import com.shepelevkirill.rksi.model.core.models.SubjectModel
 import com.shepelevkirill.rksi.model.core.processors.DateProcessor
 import com.shepelevkirill.rksi.model.core.processors.TimeProcessor
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.item_date.view.*
 import kotlinx.android.synthetic.main.item_subject.view.*
 import org.threeten.bp.LocalDate
+import java.util.concurrent.TimeUnit
 
 class ScheduleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val data: ArrayList<Any> = ArrayList()
@@ -43,6 +48,13 @@ class ScheduleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         }
     }
 
+    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+        super.onViewRecycled(holder)
+        when(holder) {
+            is SubjectViewHolder -> holder.unbind()
+        }
+    }
+
     override fun getItemViewType(position: Int): Int {
         return when (data[position]) {
             is LocalDate -> ViewHolderType.DATE.id
@@ -64,19 +76,33 @@ class ScheduleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     inner class SubjectViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        var updater: Disposable? = null
+
         fun bind(subject: SubjectModel) {
             view.subject.text = subject.subject
             view.duration.text = TimeProcessor.getDuration(subject.startTime, subject.endTime)
             view.cabinet.text = "Кабинет ${subject.cabinet}, "
             view.who.text = subject.teacher
 
-            val waitTime = TimeProcessor.getWaitTime(subject.date, subject.startTime, subject.endTime)
-            if (waitTime == "") {
-                view.waitTime.visibility = View.GONE
-            } else {
-                view.waitTime.visibility = View.VISIBLE
-                view.waitTime.text = waitTime
-            }
+            if (updater == null)
+                updater = Observable.interval(1, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.single())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        val waitTime = TimeProcessor.getWaitTime(subject.date, subject.startTime, subject.endTime)
+                        if (waitTime == "") {
+                            view.waitTime.visibility = View.GONE
+                        } else {
+                            view.waitTime.visibility = View.VISIBLE
+                            view.waitTime.text = waitTime
+                        }
+
+
+                    }
+        }
+
+        fun unbind() {
+            updater?.dispose()
         }
     }
 
